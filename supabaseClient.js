0c4e98548c835d65c9e00010a785f97d18cd4717
ZiffMafiya <zifmafiya@gmail.com>
2025-07-29 06:11:02 +0300

    1й1й

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
 // Replace with your actual Supabase URL and Anon Key
-const supabaseUrl = process.env.SUPABASE_URL || 'YOUR_SUPABASE_URL';
-const supabaseAnonKey = process.env.SUPABASE_ANON_KEY || 'YOUR_SUPABASE_ANON_KEY';
+const supabaseUrl = process.env.SUPABASE_URL || 'https://bvidmranxxoueoruaayu.supabase.co';
+const supabaseAnonKey = process.env.SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ2aWRtcmFueHhvdWVvcnVhYXl1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTMyODA2MTgsImV4cCI6MjA2ODg1NjYxOH0.EukeZ-JJTmGPlBeunHD_-pUDE7De2Pdqscclg4FVIHY';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
