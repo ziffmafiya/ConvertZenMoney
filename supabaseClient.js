dd12a6140b6295d5063b8e69756b162ac87cb4be
ZiffMafiya <zifmafiya@gmail.com>
2025-07-29 21:13:37 +0300

    1q2w3e

--- supabaseClient.js
+++
@@ -1,6 +0,0 @@
-// Replace with your actual Supabase URL and Anon Key
-const supabaseUrl = 'https://bvidmranxxoueoruaayu.supabase.co'; // Replace with your Supabase Project URL
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ2aWRtcmFueHhvdWVvcnVhYXl1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTMyODA2MTgsImV4cCI6MjA2ODg1NjYxOH0.EukeZ-JJTmGPlBeunHD_-pUDE7De2Pdqscclg4FVIHY'; // Replace with your Supabase Public Anon Key
-
-// Initialize Supabase client using the globally available 'supabase' object from the CDN
-export const supabase = window.supabase.createClient(supabaseUrl, supabaseAnonKey);
